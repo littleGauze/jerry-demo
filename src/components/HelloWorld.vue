f1cb1214521d111ccea9f4b34fffe757a0b5237a
littleGauze <810483824@qq.com>
2021-06-15 16:35:12 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,109 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="item">
+      <label for="light">灯光：</label>
+      <div class="action">
+        <p>
+          <button @click="lightHanlder('on')">开灯</button>
+          <button style="margin-left: 10px" @click="lightHanlder('off')">
+            关灯
+          </button>
+        </p>
+      </div>
+    </div>
+    <div class="item">
+      <label for="light">温湿度：</label>
+      <div class="action">
+        <p>温度: <span>{{temp}}</span> ℃</p>
+        <p style="margin-left: 40px">湿度: <span>{{humi}}</span> RH%</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+import * as ws from "websocket";
+const WebSocketClient = ws.w3cwebsocket;
+const type = {
+  LIGHT_CTRL: 'LIGHT_CTRL',
+  TEMP_HUMI: 'TEMP_HUMI'
 }
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      cient: null,
+      finished: false,
+      temp: 0,
+      humi: 0
+    };
+  },
+  mounted() {
+    const self = this
+    const client = new WebSocketClient("ws://localhost:8888/", "jerry-demo");
+    client.onerror = function () {
+      console.log("Connection Error");
+    };
+
+    client.onopen = function () {
+      console.log("WebSocket Client Connected");
+    };
+
+    client.onclose = function () {
+      console.log("jerry-demo Client Closed");
+    };
+
+    client.onmessage = function (e) {
+      if (typeof e.data === "string") {
+        console.log("Received: '" + e.data + "'");
+        self.parseMessage(JSON.parse(e.data))
+      }
+    };
+
+    this.client = client;
+  },
+  methods: {
+    sendToServer(payload) {
+       this.client.send(JSON.stringify(payload))
+    },
+    lightHanlder(state) {
+      console.log("do actoin: ", state);
+      this.sendToServer({
+        type: type.LIGHT_CTRL,
+        payload: { state }
+      })
+    },
+    parseMessage(msg) {
+      if (msg.type === type.LIGHT_CTRL) {
+        this.finished = true
+      } else {
+        this.temp = msg.payload.temp
+        this.humi = msg.payload.humi
+      }
+    }
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+div.item {
+  margin: 40px;
+}
+label {
+  display: block;
+  font-weight: bold;
+  font-size: 24px;
+  text-align: left;
+}
+div.action {
+  display: flex;
+}
+div.action p span {
+  font-size: 26px;
+  font-weight: bold;
+}
 h3 {
   margin: 40px 0 0;
 }
